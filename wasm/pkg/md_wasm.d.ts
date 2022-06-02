18c4cacbfa49f0cca6cf88e7905a2ca7666e1522
Niek Hagen <niekhagen@hotmail.com>
2022-06-02 22:56:27 +0200

    (fix): make sure built wasm is included

---
+++ wasm/pkg/md_wasm.d.ts
@@ -0,0 +1,7 @@
+/* tslint:disable */
+/* eslint-disable */
+/**
+* @param {string} markdown_input
+* @returns {string}
+*/
+export function parse_markdown(markdown_input: string): string;
---
+++ wasm/pkg/md_wasm.js
@@ -0,0 +1,2 @@
+import * as wasm from "./md_wasm_bg.wasm";
+export * from "./md_wasm_bg.js";
